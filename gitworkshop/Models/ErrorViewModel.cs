e2afe4a143fdc432cb368f5f2faa8570e8fd3f0e
g4genious <g_4_genious@yahoo.com>
2018-11-18 15:43:44 +0500

    Add project files.

---
+++ gitworkshop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace gitworkshop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
